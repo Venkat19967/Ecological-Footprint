2aafc028602e4303dd28dec968652b85841894b9
Venkat19967 <venkatss10@gmail.com>
2017-07-01 00:59:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,475 @@
+function hello()
+{
+var x=0;
+var sum=0;
+x=document.getElementById("q1").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+50;
+} else if (x==2) {
+sum=sum+70;
+} else{
+sum=sum+90;
+};
+
+x=document.getElementById("q2").selectedIndex;
+if (x==0) {
+sum=sum+40;
+} else  {
+sum=sum+20;
+};
+
+x=document.getElementById("q3").selectedIndex;
+if (x==0) {
+sum=sum+40;
+} else{
+sum=sum+0;
+
+};
+
+x=document.getElementById("q4").selectedIndex;
+if (x==0) {
+sum=sum+80;
+} else {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q5").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q6").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else{
+sum=sum+0;
+
+};
+
+x=document.getElementById("q7").selectedIndex;
+if (x==0) {
+sum=sum+50;
+} else {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q8").selectedIndex;
+if (x==0) {
+sum=sum+150;
+} else if (x==1) {
+sum=sum+100;
+} else if (x==2) {
+sum=sum+80;
+} 
+else if (x==2) {
+sum=sum+40;
+}
+else if (x==2) {
+sum=sum+40;
+}
+else if (x==2) {
+sum=sum+40;
+}
+else if (x==2) {
+sum=sum+20;
+}
+else if (x==2) {
+sum=sum+20;
+}else{
+sum=sum+20;
+};
+
+x=document.getElementById("q9").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+30;
+} else {
+sum=sum+60;
+
+};
+
+x=document.getElementById("q10").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+30;
+} else  {
+sum=sum+60;
+
+};
+
+x=document.getElementById("q11").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else {
+sum=sum+60;
+
+};
+
+x=document.getElementById("q12").selectedIndex;
+if (x==0) {
+sum=sum+100;
+} else if (x==1) {
+sum=sum+30;
+} else {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q13").selectedIndex;
+if (x==0) {
+sum=sum+100;
+} else if (x==1) {
+sum=sum+30;
+} else{
+sum=sum+0;
+
+};
+
+x=document.getElementById("q14").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+100;
+} else if (x==2) {
+sum=sum+150;
+} else{
+sum=sum+200;
+};
+
+x=document.getElementById("q15").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+5;
+} else if (x==2) {
+sum=sum+30;
+} else{
+sum=sum+200;
+};
+
+x=document.getElementById("q16").selectedIndex;
+if (x==0) {
+sum=sum-50;
+} else if (x==1) {
+sum=sum+50;
+} else if (x==2) {
+sum=sum+100;
+} else{
+sum=sum+200;
+};
+
+x=document.getElementById("q17").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+40;
+} else if (x==2) {
+sum=sum+60;
+} else{
+sum=sum+100;
+};
+
+x=document.getElementById("q18").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else if (x==1) {
+sum=sum+50;
+} else if (x==2) {
+sum=sum+100;
+} else{
+sum=sum+200;
+};
+
+x=document.getElementById("q19").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else if (x==1) {
+sum=sum+50;
+} else {
+sum=sum+100;
+
+};
+
+x=document.getElementById("q20").selectedIndex;
+if (x==0) {
+sum=sum-75;
+} else if (x==1) {
+sum=sum-25;
+} else if (x==2) {
+sum=sum+0;
+} 
+else if (x==2) {
+sum=sum+100;
+}
+else{
+sum=sum+200;
+};
+
+x=document.getElementById("q21").selectedIndex;
+if (x==0) {
+sum=sum+10;
+} else if (x==1) {
+sum=sum+80;
+} else if (x==2) {
+sum=sum+140;
+} else{
+sum=sum+200;
+};
+
+x=document.getElementById("q22").selectedIndex;
+if (x==0) {
+sum=sum-50;
+} else{
+sum=sum+0;
+
+};
+
+x=document.getElementById("q23").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+200;
+} else {
+sum=sum+400;
+
+};
+
+x=document.getElementById("q24").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else if (x==1) {
+sum=sum+50;
+} else if (x==2) {
+sum=sum+100;
+} else{
+sum=sum+150;
+};
+
+x=document.getElementById("q25").selectedIndex;
+if (x==0) {
+sum=sum-50;
+} else if (x==1) {
+sum=sum+20;
+} else {
+sum=sum+60;
+
+};
+
+x=document.getElementById("q26").selectedIndex;
+if (x==0) {
+sum=sum-50;
+} else{
+sum=sum+50;
+
+};
+
+x=document.getElementById("q27").selectedIndex;
+if (x==0) {
+sum=sum-50;
+} else{
+sum=sum+50;
+
+};
+
+x=document.getElementById("q28").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else{
+sum=sum+50;
+
+};
+
+x=document.getElementById("q29").selectedIndex;
+if (x==0) {
+sum=sum+60;
+} else if (x==1) {
+sum=sum-10;
+} else {
+sum=sum-50;
+
+};
+
+x=document.getElementById("q30").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+10;
+} else if (x==2) {
+sum=sum+20;
+} else{
+sum=sum+100;
+};
+
+x=document.getElementById("q31").selectedIndex;
+if (x==0) {
+sum=sum+80;
+} else{
+sum=sum+0;
+
+};
+
+x=document.getElementById("q32").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else  {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q33").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else{
+sum=sum+0;
+
+};
+
+x=document.getElementById("q34").selectedIndex;
+if (x==0) {
+sum=sum+120;
+} else {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q35").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else {
+sum=sum+100;
+
+};
+
+x=document.getElementById("q36").selectedIndex;
+if (x==0) {
+sum=sum-10;
+} else  {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q37").selectedIndex;
+if (x==0) {
+sum=sum+25;
+} else if (x==1) {
+sum=sum+50;
+} else {
+sum=sum+75;
+
+};
+
+x=document.getElementById("q38").selectedIndex;
+if (x==0) {
+sum=sum+20;
+} else if (x==1) {
+sum=sum+60;
+} else {
+sum=sum+90;
+
+};
+
+x=document.getElementById("q39").selectedIndex;
+if (x==0) {
+sum=sum+20;
+} else if (x==1) {
+sum=sum+60;
+} else if (x==2) {
+sum=sum+200;
+} else{
+sum=sum-50;
+};
+
+x=document.getElementById("q40").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q41").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else{
+sum=sum+0;
+
+};
+
+x=document.getElementById("q42").selectedIndex;
+if (x==0) {
+sum=sum-20;
+} else {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q43").selectedIndex;
+if (x==0) {
+sum=sum-10;
+} else {
+sum=sum+60;
+
+};
+
+x=document.getElementById("q44").selectedIndex;
+if (x==0) {
+sum=sum-30;
+} else  {
+sum=sum+0;
+
+};
+
+x=document.getElementById("q45").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+20;
+} else if (x==2) {
+sum=sum+60;
+} else{
+sum=sum+100;
+};
+
+x=document.getElementById("q46").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+50;
+} else  {
+sum=sum+80;
+
+};
+
+x=document.getElementById("q47").selectedIndex;
+if (x==0) {
+sum=sum+0;
+} else if (x==1) {
+sum=sum+20;
+} else if (x==2) {
+sum=sum+60;
+} else{
+sum=sum+80;
+};
+
+sum=sum/100*2.47/4.7;
+sum=Math.abs(sum);
+sum=sum.toFixed(2);
+document.getElementById("answer").innerHTML=sum;
+
+
+}
+function hello2()
+{
+
+prompt("hello");
+
+}
